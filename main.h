1d82b7b8b8e0ccb2a48fc4059345b424683c76e5
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-10-19 19:26:23 +0100

    Inject

--- main.h
+++ main.h
@@ -38,7 +38,7 @@ struct fmt
  * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
-
+int _putchar(char c);
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
