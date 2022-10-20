f157c8da7e49b388de45946fb8d593e1f5d20892
Olajide Samson Yomi <olajideyormith96@gmail.com>
2022-10-20 13:17:12 -0700

    printf project

--- _putchar.c
+++ _putchar.c
@@ -1,82 +1,13 @@
-#include "main.h"
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: pointer to valid function or NULL
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-static int (*check_for_specifiers(const char *format))(va_list)
+int _putchar(char c)
 {
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
-
-	for (i = 0; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		{
-			break;
-		}
-	}
-	return (p[i].f);
-}
-
-/**
- * _printf - prints anything
- * @format: list of argument types passed to the function
- *
- * Return: number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
-
-	if (format == NULL)
-		return (-1);
-	va_start(valist, format);
-	while (format[i])
-	{
-		for (; format[i] != '%' && format[i]; i++)
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		if (!format[i])
-			return (count);
-		f = check_for_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(valist);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		count++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
-	}
-	va_end(valist);
-	return (count);
+	return (write(1, &c, 1));
 }
